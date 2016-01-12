5ef1698b4edf593bd556085a78bad756a1d6575c
root <root@localhost.localdomain>
2016-01-11 19:28:09 -0500

    initial check of updateGit

--- watcher.php
+++ watcher.php
@@ -1,3 +1,19 @@
 <?php
 
+updateGit('initial check of updateGit');
 
+
+function updateGit($message) {
+	$dirs = array("md5", "raw", ".");
+	foreach ($dirs as $dir) {
+		$dh = opendir($dir);
+		while (false !== ($filename = readdir($dh))) {
+			if (($filename <> '.') && ($filename <> '..')) {
+				$cmd[] = 'git add ' . $dir . '/' . $filename;
+			}
+		}
+	}
+	$cmd[] = 'git commit -m "' . $message . '"';
+	$cmd[] = 'git push -u origin master';
+	foreach ($cmd as $c) echo $c . "\n" . trim(`$c`) . "\n";
+}
