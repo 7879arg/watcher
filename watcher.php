100483e059a4178d60570d67fada2bd822fdfcee
root <root@localhost.localdomain>
2016-01-11 19:50:29 -0500

    Found change on

--- watcher.php
+++ watcher.php
@@ -1,7 +1,28 @@
 <?php
 
-updateGit('initial check of updateGit');
+$urls = file('urls.txt');
+$updatemessage = '';
+foreach ($urls as $url) {
+	$url = trim($url); $filename = urlencode($url);
+	$oldmd5 = implode(file('md5/' . $filename));
+	$md5 = md5(implode(file($url)));
 
+	if ($oldmd5 <> $md5) {
+		$updatemessage = "Found change on " . $url . "\n";
+		$fp = fopen('md5/' . $filename, 'w+');
+		fwrite($fp, $md5);
+		fclose($fp);
+		$fp = fopen('raw/' . $filename, 'w+');
+		fwrite($fp, file($url));
+		fclose($fp);
+	}
+
+}
+
+if ($updatemessage <> '') {
+	updateGit($updatemessage);
+	echo $updatemessage;
+}
 
 function updateGit($message) {
 	$dirs = array("md5", "raw", ".");
